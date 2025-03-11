0f01ebaf748cd532784dba641497fb41fe688a1c
matandol <mibins210@gmail.com>
2025-03-10 23:04:10 -0400

    Actualizar __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 LOGS = logging.getLogger(__name__)
 
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('API_ID', CONFIG['api_id']))
+API_ID = int(os.getenv('8095321680:AAFv7L8UwlrSDzGWeWfIpzn6bRB4V2QhVDQ', CONFIG['8095321680:AAFv7L8UwlrSDzGWeWfIpzn6bRB4V2QhVDQ']))
 API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
 DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
 STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
