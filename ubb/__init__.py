174571f2aca1b9ee736ac766419323cac85ac2ca
matandol <mibins210@gmail.com>
2025-03-10 23:06:31 -0400

    Actualizar __init__.py
    y

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -3,21 +3,6 @@ import logging
 import yaml
 
 from telethon import TelegramClient
-from telethon.sessions import StringSession
-
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
-LOGS = logging.getLogger(__name__)
-
-CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('8095321680:AAFv7L8UwlrSDzGWeWfIpzn6bRB4V2QhVDQ', CONFIG['8095321680:AAFv7L8UwlrSDzGWeWfIpzn6bRB4V2QhVDQ']))
-API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
-DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
-STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
-
-Ubot = TelegramClient(StringSession(STRING_SESSION),
-                      API_ID,
-                      API_HASH,
+from telethon.sessions      API_HASH,
                       auto_reconnect=False,
                       lang_code='en')
